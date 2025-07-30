0e6f1c17f9b3b59c456cc8475603357b1bfa4a9e
froggitti <85637569+froggitti@users.noreply.github.com>
2025-07-30 18:47:49 -0400

    Update get-deps.sh

--- get-deps.sh
+++ get-deps.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
-mkdir ~/.anki/vicos-sdk/dist/5.2.1-r06
-
-cd ~/.anki/vicos-sdk/dist/5.2.1-r06
-
-wget https://froggitti.net/5.2.1-r06.tar.gz
-
-gunzip 5.2.1-r06.tar.gz
-
-tar -xvf 5.2.1-r06.tar
+if [[ ! -f ~/.anki/vicos-sdk/dist/5.2.1-r06 ]]; then
+  echo Getting deps...
+  mkdir ~/.anki/vicos-sdk/dist/5.2.1-r06
+  cd ~/.anki/vicos-sdk/dist/5.2.1-r06
+  wget https://froggitti.net/5.2.1-r06.tar.gz
+  gunzip 5.2.1-r06.tar.gz
+  tar -xvf 5.2.1-r06.tar
+else
+  echo You already have the 5.2.1-r06 toolchain installed. Exiting...
+  exit
+fi
