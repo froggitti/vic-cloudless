88f0247934811f8c4ef45dcc401458e3cfba0c7b
froggitti <85637569+froggitti@users.noreply.github.com>
2025-07-30 18:44:57 -0400

    Create get-deps.sh

---
+++ get-deps.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+mkdir ~/.anki/vicos-sdk/dist/5.2.1-r06
+
+cd ~/.anki/vicos-sdk/dist/5.2.1-r06
+
+wget https://froggitti.net/5.2.1-r06.tar.gz
+
+gunzip 5.2.1-r06.tar.gz
+
+tar -xvf 5.2.1-r06.tar
