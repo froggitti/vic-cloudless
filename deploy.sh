0dcce5c0e86b034b57a2d7cb5d31f3a64b7c668c
Wire <kerigancreighton@gmail.com>
2025-07-30 18:43:34 -0500

    we do NOT need -O like OMG froggitti WOW

--- deploy.sh
+++ deploy.sh
@@ -19,9 +19,9 @@ else
 fi
 
 ssh root@$1 "systemctl stop anki-robot.target && mount -o rw,remount / && rm -rf /anki/data/assets/cozmo_resources/cloudless && mkdir -p /anki/data/assets/cozmo_resources/cloudless"
-scp -O build/vic-cloud root@$1:/anki/bin/
-scp -O build/lib* root@$1:/anki/lib/
-scp -O extra/cloud.sudoers root@$1:/etc/sudoers.d/cloud
-scp -O extra/setfreq root@$1:/usr/sbin/
+scp build/vic-cloud root@$1:/anki/bin/
+scp build/lib* root@$1:/anki/lib/
+scp extra/cloud.sudoers root@$1:/etc/sudoers.d/cloud
+scp extra/setfreq root@$1:/usr/sbin/
 rsync -avr build/en-US root@$1:/anki/data/assets/cozmo_resources/cloudless/
 ssh root@$1 "chmod +rwx /usr/sbin/setfreq && systemctl daemon-reload && sudo -k && systemctl start anki-robot.target"
