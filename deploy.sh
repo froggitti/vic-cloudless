d7caa407f0d14b63aa60499a2ecdd614c3a7d853
froggitti <folipayt@gmail.com>
2025-07-30 19:37:12 -0400

    not gonna bother with this for now

--- deploy.sh
+++ deploy.sh
@@ -23,8 +23,5 @@ scp -O build/vic-cloud root@$1:/anki/bin/
 scp -O build/lib* root@$1:/anki/lib/
 scp -O extra/cloud.sudoers root@$1:/etc/sudoers.d/cloud
 scp -O extra/setfreq root@$1:/usr/sbin/
-scp -O rsync/rsync root@$1:/usr/bin/
-scp -O rsync/rsyncd.conf root@$1:/run/systemd/system/rsyncd.conf
-scp -O rsync/rsyncd.service root@$1:/lib/systemd/system/
 rsync -avr build/en-US root@$1:/anki/data/assets/cozmo_resources/cloudless/
 ssh root@$1 "chmod +rwx /usr/sbin/setfreq && systemctl daemon-reload && sudo -k && systemctl start anki-robot.target"
