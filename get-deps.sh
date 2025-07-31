0c26a4c1e52017de61cece0dd8976aa9a2cd0ac7
froggitti <folipayt@gmail.com>
2025-07-30 20:08:44 -0400

    fix incorrect file name

--- get-deps.sh
+++ get-deps.sh
@@ -5,8 +5,8 @@ if [[ ! -d ~/.anki/vicos-sdk/dist/1.1.0-r04 ]]; then
   mkdir ~/.anki/vicos-sdk/dist/1.1.0-r04
   cd ~/.anki/vicos-sdk/dist/1.1.0-r04
   wget https://github.com/kercre123/anki-deps/releases/download/1.1.0-r04/vicos-sdk-1.1.0-r04-x86_64-ubuntu-16.04.tar.gz
-  gunzip 1.1.0-r04.tar.gz
-  tar -xvf 1.1.0-r04.tar
+  gunzip vicos-sdk-1.1.0-r04-x86_64-ubuntu-16.04.tar.gz
+  tar -xvf vicos-sdk-1.1.0-r04-x86_64-ubuntu-16.04.tar
 else
   echo You already have the 1.1.0-r04 toolchain installed. Exiting...
   exit
